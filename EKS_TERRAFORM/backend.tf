2fb50d9c815eb4b29aa6f6c1d39005c3d61b4f9e
shivaramguguloth <shivaramguguloth24@gmail.com>
2025-09-20 11:06:44 +0530

    Update backend.tf
    S3 bucket name chenged

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "docker847" # Replace with your actual S3 bucket name
+    bucket = "hotstar45" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
